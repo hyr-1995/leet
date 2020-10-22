d119d76717a2eaf6882b9d1018bd2df18874f56f
Yr H <550837024@qq.com>
2020-10-22 23:28:17 +0800

    update reverse_k_group for correct

--- python/reverse_k_group.py
+++ python/reverse_k_group.py
@@ -1,8 +1,9 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         if k==1:
@@ -13,13 +14,15 @@ class Solution:
         curr = dumy
         left = curr.next
         right = left
-        
+
         while 1:
             count = 0
             
             while(count < k and right is not None):
                 count += 1
+                print(right.val)
                 right = right.next 
+            print("!!!!!!",left.val, right.val)
             if count == k:
                 # reverse
                 # next_list = right.next
@@ -30,10 +33,35 @@ class Solution:
                     cur.next = pre
                     pre = cur
                     cur = temp
-                curr.next = right 
+                curr.next = pre #!!!!!!!!!!!!
                 curr = left 
                 left = right
 
             else:
                 return dumy.next
+
+
+def main():
+    
+    temp = ListNode(1)
+    inputs = head = temp
+    for i in range(2,10):
+        temp.next = ListNode(i)
+        temp = temp.next
+
+    while(head is not None):
+        print(head.val)
+        head = head.next
+        
+    print("=================================")
+    s = Solution()
+    res = s.reverseKGroup(inputs, 2)
+
+    while(res.next is not None):
+        print(res.val)
+        res = res.next
+        
+
+if __name__ == '__main__':
+    main()
         
